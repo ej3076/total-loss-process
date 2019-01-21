eac61c3411f336ee63ff82df2da076bd944f40ff
Seamus Sloan <seamus@Seamuss-MacBook-Pro.local>
2019-01-20 22:44:11 -0500

    Replaced 'Material' with 'Alyle'

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  LyThemeModule,
+  LY_THEME
+} from '@alyle/ui';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material.module';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyToolbarModule } from '@alyle/ui/toolbar';
+import { LyResizingCroppingImageModule } from '@alyle/ui/resizing-cropping-images';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+//import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
@@ -22,10 +30,12 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
+    LyThemeModule.setTheme('minima-light'),
+    LyButtonModule,
+    LyToolbarModule,
+    LyResizingCroppingImageModule
   ],
-  providers: [],
+  providers: [{ provide: LY_THEME, useClass: MinimaLight, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
