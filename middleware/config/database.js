00bd3b8eb2b2545d082a5abdd031934b73d90bdc
Derek P Sifford <dereksifford@gmail.com>
2019-01-28 22:56:21 -0500

    dockerize the middleware services

--- middleware/config/database.js
+++ middleware/config/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
 
+const DB_NAME = process.env.DB_NAME || 'fordtotalloss';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASS = process.env.DB_PASS || 'root';
+
 // Export variable = DB name, username, password
-module.exports = new Sequelize('fordtest', 'root', '', {
-  host: 'localhost',
+module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: 'middleware-db',
+  port: 3306,
   dialect: 'mysql',
   operatorsAliases: false,
 
@@ -10,6 +15,6 @@ module.exports = new Sequelize('fordtest', 'root', '', {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
