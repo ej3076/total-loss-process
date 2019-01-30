e4f54a8faa9869d549c35e1c99bf0327199967ba
ej3076 <ej3076@wayne.edu>
2019-01-30 00:01:55 -0500

    Create form for new claims, add Ford logo, make some things prettier

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { CallbackComponent } from './callback/callback.component';
+import { NewClaimComponent } from './new-claim/new-claim.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}, 
-  {path: 'header', component: HeaderComponent},
-  {path: 'footer', component: FooterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'new-claim', component: NewClaimComponent},
   {path: 'callback', component: CallbackComponent},
   {path: '**', component: HomeComponent}
 ];
