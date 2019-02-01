847db2d9fb9712abfd39f06927b0cb3192e40742
Derek P Sifford <dereksifford@gmail.com>
2019-01-31 20:20:50 -0500

    remove test code

--- middleware/app.js
+++ middleware/app.js
@@ -32,26 +32,3 @@ app.use('/keys', require('./routes/keys'));
 app.listen(PORT, () =>
   console.log(`Server listening on port ${IS_PRODUCTION ? PORT : '8080'}.`),
 );
-
-(async () => {
-  const client = new VehicleClient(
-    'b2ac25d22f41f72fcc0ffd57e69285db485944e3bfd1e5534c8a9930a41b43cf',
-  );
-  const vehicle = {
-    vin: '67890',
-    color: 'yellow',
-    model: 'coupe',
-    status: 0,
-  };
-  var response = await client.createVehicle(vehicle);
-  console.log(response);
-
-  // var response1 = await client.editVehicle({ vin: vehicle.vin, color: 'blue' });
-  // console.log(response1);
-
-  // var response2 = await client.getVehicle(vehicle.vin);
-  // console.log(response2);
-
-  var response3 = await client.listVehicles();
-  console.log(response3);
-})();
