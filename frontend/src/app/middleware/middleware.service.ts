dc0105e03b45a79f92e1e094530a3e5d4a71ac1e
Derek P Sifford <dereksifford@gmail.com>
2019-02-05 17:59:59 -0500

    fix file I broke in merge conflict fix

--- frontend/src/app/middleware/middleware.service.ts
+++ frontend/src/app/middleware/middleware.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Vehicle } from '../models/Vehicle';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../models/User';
+import { Observable } from 'rxjs';
 
 interface KeypairResponse {
   public_key: string;
@@ -12,7 +16,16 @@ const API_BASE = 'http://localhost:8080';
   providedIn: 'root',
 })
 export class MiddlewareService {
-  constructor(private http: HttpClient) {}
+  private claims: Vehicle[];
+
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
+  get user() {
+    if (this.auth.isAuthenticated()) {
+      return this.auth.getUser();
+    }
+    throw new Error('User needs to be logged in');
+  }
 
   generateKeypair() {
     return this.http.post<KeypairResponse>(`${API_BASE}/keys/generate`, null);
@@ -32,5 +45,27 @@ export class MiddlewareService {
 
   checkDelete() {
     return this.http.delete(API_BASE);
+  }
+
+  getClaims(): Observable<Vehicle[]> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Authorization', this.auth.accessToken);
+    headers = headers.append('private_key', this.user.privateKey);
+
+    return this.http.get<Vehicle[]>(`${API_BASE}/vehicles`, { headers });
+  }
+
+  async addClaim(vehicle: Vehicle) {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append(
+      'Authorization',
+      `Bearer ${this.auth.accessToken}`,
+    );
+    headers = headers.append('private_key', this.user.privateKey);
+    return this.http
+      .post(`${API_BASE}/vehicles`, vehicle, {
+        headers,
+      })
+      .subscribe();
   }
 }
