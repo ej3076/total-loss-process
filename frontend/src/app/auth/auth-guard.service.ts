3b5dcae2ccce4bbcbaff1a0f1026d1065641bc9e
Jake Byrd <JakeByrd@quickenloans.com>
2019-02-12 18:29:23 -0500

    Remove unused import

--- frontend/src/app/auth/auth-guard.service.ts
+++ frontend/src/app/auth/auth-guard.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import { CanActivate } from '@angular/router/src/utils/preactivation';
 
 @Injectable({
   providedIn: 'root',
