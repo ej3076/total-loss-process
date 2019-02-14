d0dd2c7277e63462887a09dfee9aa05c56fdeb86
Jake Byrd <JakeByrd@quickenloans.com>
2019-02-13 23:08:48 -0500

    Rewrite sign in, show error and no claims messaging on frontend

--- frontend/src/app/view-claims/view-claims.component.ts
+++ frontend/src/app/view-claims/view-claims.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { LyTheme2 } from '@alyle/ui';
 import { MiddlewareService } from '../middleware/middleware.service';
 import { AuthService } from '../auth/auth.service';
+import { LySnackBarDismiss } from '@alyle/ui/snack-bar';
+import { LySnackBarModule } from '@alyle/ui/snack-bar';
 
 const STYLES = {
   paper: {
@@ -9,6 +11,11 @@ const STYLES = {
     margin: '.5em',
     padding: '1em',
   },
+  noClaims: {
+    display: 'block',
+    margin: '3rem auto auto auto',
+    padding: '1em',
+  }
 };
 
 @Component({
@@ -20,6 +27,8 @@ export class ViewClaimsComponent implements OnInit {
   readonly classes = this._theme.addStyleSheet(STYLES);
 
   private claims: Protos.Claim[];
+  private responseError: boolean;
+  private errorText: string;
 
   constructor(
     private _theme: LyTheme2,
@@ -28,47 +37,27 @@ export class ViewClaimsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    if (this.auth.isAuthenticated()) {
-      this.middlewareService.getClaims().subscribe(data => {
-        this.claims = data;
-      });
-    } else {
-      // FIXME: Pitch this
-      this.claims = [
-        {
-          files: [],
-          status: 0,
-          vehicle: {
-            vin: '112233445566',
-            model: 'TEST MODEL 1',
-            color: 'TEST COLOR 1',
-            year: 2001,
-            miles: 100000,
-          },
-        },
-        {
-          files: [],
-          status: 0,
-          vehicle: {
-            vin: '223344556677',
-            model: 'TEST MODEL 2',
-            color: 'TEST COLOR 2',
-            year: 2002,
-            miles: 200000,
-          },
-        },
-        {
-          files: [],
-          status: 0,
-          vehicle: {
-            vin: '334455667788',
-            model: 'TEST MODEL 3',
-            color: 'TEST COLOR 3',
-            year: 2003,
-            miles: 300000,
-          },
-        },
-      ];
+    if (this.auth.loggedIn) {
+      this.setClaims();
     }
+  }
+
+  afterDismissed(e: LySnackBarDismiss) {
+    console.log(e);
+  }
+
+  setClaims(): void {
+    this.middlewareService.getClaims().subscribe(
+      result => { this.claims = result; },
+      error => { 
+        this.responseError = true; 
+        this.errorText = error;
+      },
+      () => {});
+  }
+
+  openSnackBar() {
+
+    
   }
 }
