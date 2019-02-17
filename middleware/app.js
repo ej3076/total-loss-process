720e50f08f65dc25b552104fc5abbfd663dc30b4
Derek Sifford <dereksifford@gmail.com>
2019-02-17 18:10:00 -0500

    split routes into multiple files + make custom auth middleware (#34)
    * split routes into multiple files + make custom auth middleware
    
    * fix typo in route

--- middleware/app.js
+++ middleware/app.js
@@ -3,101 +3,16 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-// TODO: Use this for scoping endpoints...
-// const jwtAuthz = require('express-jwt-authz');
-
-const ClaimClient = require('./lib/claim-client');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Authentication middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: false,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://total-loss-process.auth0.com/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer.
-  audience: 'https://total-loss-process.auth0.com/api/v2/',
-  issuer: `https://total-loss-process.auth0.com/`,
-  algorithms: ['RS256'],
-});
-
 app.use(express.json());
+// TODO: Make this strictly only accept CORS from the web service address.
 app.use(cors());
 
-// Insert a new claim into the blockchain.
-app.post('/claims', checkJwt, async (req, res) => {
-  try {
-    const privateKey = req.headers.private_key;
-    if (typeof privateKey !== 'string') {
-      throw new Error('Invalid request');
-    }
-    const client = new ClaimClient(privateKey);
-    const response = await client.createClaim(req.body);
-    console.log(response);
-    res.send(response);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-// Edit an existing claim on the blockchain using VIN.
-app.post('/claims/:vin', checkJwt, async (req, res) => {
-  try {
-    const privateKey = req.headers.private_key;
-    if (typeof privateKey !== 'string') {
-      throw new Error('Invalid request');
-    }
-    const client = new ClaimClient(privateKey);
-    const response = await client.editClaim(req.params.vin, req.body);
-    console.log(response);
-    res.send(response);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-// Retrieve a claim from the blockchain using VIN.
-app.get('/claims/:vin', async (req, res) => {
-  try {
-    const vin = req.params.vin;
-    const client = new ClaimClient();
-    const claimList = await client.getClaim(vin);
-    console.log(claimList);
-    res.send(claimList);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
-// Retrieve a list of claims from the blockchain.
-app.get('/claims', async (_, res) => {
-  try {
-    const client = new ClaimClient();
-    const claimsList = await client.listClaims();
-    console.log(claimsList);
-    res.send(claimsList);
-  } catch (err) {
-    console.log(err);
-    res.sendStatus(500);
-  }
-});
-
+app.use('/claims', require('./routes/claims'));
 app.use('/keys', require('./routes/keys'));
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}.`));
