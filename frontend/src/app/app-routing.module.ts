ad22a428215fc4091fe43bdec7f237ba997b7bd8
Jake Byrd <JakeByrd@quickenloans.com>
2019-02-17 17:07:11 -0500

    Create single claim view with getClaim and addFiles support.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,18 +5,23 @@ import { CallbackComponent } from './callback/callback.component';
 import { NewClaimComponent } from './new-claim/new-claim.component';
 import { ViewClaimsComponent } from './view-claims/view-claims.component';
 import { AuthGuardService } from './auth/auth-guard.service';
+import { EditClaimComponent } from './edit-claim/edit-claim.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {
-    path: 'new-claim',
+    path: 'add',
     component: NewClaimComponent,
     canActivate: [AuthGuardService],
   },
   {
-    path: 'view-claims',
+    path: 'claims',
     component: ViewClaimsComponent,
     canActivate: [AuthGuardService],
+  },
+  {
+    path: 'claims/:vin',
+    component: EditClaimComponent
   },
   { path: 'callback', component: CallbackComponent },
   { path: '**', component: HomeComponent },
