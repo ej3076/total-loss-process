470c33573a87c2ab8cae02d0462157b6cc98165d
Jake Byrd <JakeByrd@quickenloans.com>
2019-02-18 20:20:35 -0500

    Add redirect on successful claim creation, along with a log on redirect (if failed or successful) - also make file uploads keyed

--- frontend/src/app/middleware/middleware.service.ts
+++ frontend/src/app/middleware/middleware.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
 import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 interface KeypairResponse {
   public_key: string;
@@ -18,7 +19,7 @@ export class MiddlewareService {
 
   private x: Protos.Claim;
 
-  constructor(private http: HttpClient, private auth: AuthService) {}
+  constructor(private http: HttpClient, private auth: AuthService, private router: Router) {}
 
   get user() {
     if (this.auth.loggedIn) {
@@ -50,24 +51,24 @@ export class MiddlewareService {
   getClaim(vin: string): Observable<Protos.Claim> {
 
     // TODO: Remove mocked data.
-    // this.x = {
-    //   vehicle: {
-    //     vin: vin,
-    //     model: "Escape",
-    //     color: "Red",
-    //     year: 2005,
-    //     miles: 100023
-    //   },
-    //   status: 0,
-    //   files: [
-    //     {
-    //       hash: "1237-1232-1231fsef-123-sgseg",
-    //       name: "police-report.png"
-    //     }
-    //   ]
-    // }
+    this.x = {
+      vehicle: {
+        vin: vin,
+        model: "Escape",
+        color: "Red",
+        year: 2005,
+        miles: 100023
+      },
+      status: 0,
+      files: [
+        {
+          hash: "1237-1232-1231fsef-123-sgseg",
+          name: "police-report.png"
+        }
+      ]
+    }
 
-    // return of<Protos.Claim> (this.x);
+    return of<Protos.Claim> (this.x);
 
     return this.http.get<Protos.Claim>(`${API_BASE}/claims/${vin}`, {
       headers: this.headers
@@ -79,14 +80,32 @@ export class MiddlewareService {
       .post(`${API_BASE}/claims`, claim, {
         headers: this.headers,
       })
-      .subscribe();
+      .subscribe(
+        success => {
+          // Log when successful / errored navigation
+          this.router.navigate([`/claims/${claim.vehicle.vin}`]).then(
+            nav => {
+              console.log(nav);
+            },
+            error => {
+              console.log(error)
+            }
+          );
+        }
+      );
   }
 
   addFiles(files: FileList, vin: string) {
     let headers = this.headers;
     headers.append('Content-Type', 'multipart/form-data');
 
-    return this.http.post(`${API_BASE}/claims/${vin}/files`, files, {
+    const data = new FormData();
+
+    for (var i = 0; i < files.length; i++) {
+      data.append('files[]', files.item(i));
+    }
+
+    return this.http.post(`${API_BASE}/claims/${vin}/files`, data, {
       headers: headers
     });
   }
