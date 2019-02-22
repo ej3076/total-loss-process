ccec20578750c8366fb9504097eac5a536bbdbac
Seamus Sloan <seamus@Seamuss-MacBook-Pro.local>
2019-02-21 23:33:31 -0500

    Finished the claims sorting on View Claims. Fixed an issue with footer not sticking to bottom when more than 6 claims
    Added routerLink to Ford Logo in right corner.
    (Disclaimer: There's a blue rectangle around the logo after clicking it. Unsure of what that is)

--- frontend/src/app/view-claims/view-claims.component.ts
+++ frontend/src/app/view-claims/view-claims.component.ts
@@ -25,10 +25,7 @@ export class ViewClaimsComponent implements OnInit {
   readonly classes = this._theme.addStyleSheet(STYLES);
 
   private claims: Protos.Claim[] = [];
-  private claims0: Protos.Claim[] = [];
-  private claims1: Protos.Claim[] = [];
-  private claims2: Protos.Claim[] = [];
-  private claims3: Protos.Claim[] = [];
+  private visibleClaims: Protos.Claim[] = [];
   private responseError: boolean;
   private errorText: string;
 
@@ -41,6 +38,7 @@ export class ViewClaimsComponent implements OnInit {
   ngOnInit() {
     if (this.auth.loggedIn) {
       this.setClaims();
+      this.sortClaims(0);
     }
   }
 
@@ -54,22 +52,7 @@ export class ViewClaimsComponent implements OnInit {
       () => {});
   }
 
-  sortClaims(num): void{
-    var i: number;
-    for(i = num - 1; i >= 0; i--){
-      if(this.claims[i].status == 0){
-        console.log("I got one!");
-        this.claims0.push(this.claims[i])
-      }
-      if(this.claims[i].status == 1){
-        this.claims1.push(this.claims[i])
-      }
-      if(this.claims[i].status == 2){
-        this.claims2.push(this.claims[i])
-      }
-      if(this.claims[i].status == 3){
-        this.claims3.push(this.claims[i])
-      }
-    }
+  sortClaims(num): Protos.Claim[] {
+    return this.claims.filter(claim => claim.status === num);
   }
-}
+  }
