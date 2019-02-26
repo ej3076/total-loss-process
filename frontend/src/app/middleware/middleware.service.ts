fff98d65ae517638685973184a1f2b5ef961b48e
ej3076 <46799635+ej3076@users.noreply.github.com>
2019-02-26 13:39:05 -0500

    Single Claim View (#44)
    * Make single claim view page
    
    * Cleanup
    
    * fixes from code review
    
    * Fixes

--- frontend/src/app/middleware/middleware.service.ts
+++ frontend/src/app/middleware/middleware.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
 import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
+import { concatMap } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -51,27 +52,6 @@ export class MiddlewareService {
   }
 
   getClaim(vin: string): Observable<Protos.Claim> {
-
-    // TODO: Remove mocked data.
-    // this.x = {
-    //   vehicle: {
-    //     vin: vin,
-    //     model: "Escape",
-    //     color: "Red",
-    //     year: 2005,
-    //     miles: 100023
-    //   },
-    //   status: 0,
-    //   files: [
-    //     {
-    //       hash: "1237-1232-1231fsef-123-sgseg",
-    //       name: "police-report.png"
-    //     }
-    //   ]
-    // }
-
-    // return of<Protos.Claim> (this.x);
-
     return this.http.get<Protos.Claim>(`${API_BASE}/claims/${vin}`, {
       headers: this.headers
     });
@@ -82,20 +62,29 @@ export class MiddlewareService {
       .post(`${API_BASE}/claims`, claim, {
         headers: this.headers,
       })
+      .subscribe( 
+        data => {},
+        undefined,
+        () => this.router.navigate([`/claims/${claim.vehicle.vin}`])
+      );
+  };
+
+  editClaim(claim: DeepPartial<Protos.Claim>) {
+    console.log(claim);
+    return this.http
+      .post(`${API_BASE}/claims/${claim.vehicle.vin}`, claim, {
+        headers: this.headers
+      })
       .subscribe(
-        success => {
-          // Log when successful / errored navigation
-          this.router.navigate([`/claims/${claim.vehicle.vin}`]).then(
-            nav => {
-              console.log(nav);
-            },
-            error => {
-              console.log(error)
-            }
-          );
+        data => {},
+        error => {alert("Edit failed, please check console log")},
+        () => {
+          alert("Edit successful!");
+          this.router.navigateByUrl('/home', {skipLocationChange: true}).then(()=>
+            this.router.navigate([`/claims/${claim.vehicle.vin}`]));
         }
       );
-  }
+  };
 
   addFiles(files: FileList, vin: string) {
     let headers = this.headers;
@@ -109,6 +98,20 @@ export class MiddlewareService {
 
     return this.http.post(`${API_BASE}/claims/${vin}/files`, data, {
       headers: headers
+    })
+    .subscribe(
+      data => {},
+      undefined,
+      () => {
+        alert("File upload success!");
+
+      }
+    );
+  }
+
+  deleteFile(hash: string, vin: string) {
+    return this.http.delete(`${API_BASE}/claims/${vin}/files/${hash}`, {
+      headers: this.headers
     });
   }
 }
