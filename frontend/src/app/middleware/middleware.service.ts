2729db8a6bcc373f71f701654640d70807e4df17
Jake Byrd <JakeByrd@quickenloans.com>
2019-03-02 14:10:14 -0500

    Revert unecessary import reorder

--- frontend/src/app/middleware/middleware.service.ts
+++ frontend/src/app/middleware/middleware.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 import { AuthService } from '../auth/auth.service';
