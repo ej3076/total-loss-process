81b0a3d9d6fdd5d8ecb6e617000b3554b415f515
Derek Sifford <dereksifford@gmail.com>
2019-03-13 11:10:52 -0400

    update protos

--- types/protos.d.ts
+++ types/protos.d.ts
@@ -1,41 +1,167 @@
 declare namespace Protos {
-  namespace File {
+  namespace Claim {
     enum Status {
-      ACTIVE,
-      ARCHIVED,
+      /**
+       * Initial data entry and settlement agreement being orchestrated by the
+       * insurance company.
+       */
+      SETTLEMENT,
+      /**
+       * Settlement has been reached. Information and files are being
+       * collected/requested by agencies exterior to insurance company (DMV,
+       * Ford Credit, etc).
+       */
+      DATA_COLLECTION,
+      /**
+       * All required files have been obtained. Vehicle is ready for auction.
+       */
+      AUCTION,
+      /**
+       * Vehicle has passed auction phase. Case has now been fully resolved.
+       */
+      RESOLVED,
     }
   }
-  interface File {
-    status: File.Status;
-    hash: string;
-    name: string;
-  }
-
   interface Claim {
-    files: File[];
-    status: number;
+    /**
+     * The status of the vehicle in the system.
+     */
+    status: Claim.Status;
+    /**
+     * JSON serialized UTC date representing claim creation time.
+     */
+    created: string;
+    /**
+     * JSON serialized UTC date representing last modification time.
+     */
+    modified: string;
+    /**
+     * JSON serialized UTC date representing the date of vehicle loss.
+     */
+    date_of_loss: string;
+    /**
+     * The vehicle info.
+     */
     vehicle: Vehicle;
-  }
-
-  interface Vehicle {
-    vin: string;
-    model: string;
-    year: number;
-    miles: number;
-    color: string;
-    //acv: number;
+    /**
+     * The insurer info.
+     */
+    insurer: Insurer;
+    /**
+     * An array of file protos.
+     */
+    files: File[];
   }
 
   namespace ClaimPayload {
     enum Action {
+      /**
+       * Ensures that payload actions are always specified explicitly. Should not be used on purpose.
+       */
       ERROR,
+      /**
+       * Create a new claim.
+       */
       CREATE_CLAIM,
+      /**
+       * Delete an existing claim.
+       */
       DELETE_CLAIM,
+      /**
+       * Edit an existing claim.
+       */
       EDIT_CLAIM,
     }
   }
   interface ClaimPayload {
+    /**
+     * Action to be taken.
+     */
     action: ClaimPayload.Action;
+    /**
+     * Claim data associated with the action.
+     */
     data: Claim;
   }
+
+  namespace File {
+    enum Status {
+      /**
+       * File is active and visible.
+       */
+      ACTIVE,
+      /**
+       * File has been archived by the user.
+       */
+      ARCHIVED,
+    }
+    enum Type {
+      /**
+       * Misc documents/images.
+       */
+      NONE,
+      /**
+       * Power of Attorney.
+       */
+      POA,
+      /**
+       * Vehicle title.
+       */
+      TITLE,
+      /**
+       * Odometer disclosure statement.
+       */
+      ODS,
+      /**
+       * Police report.
+       */
+      POLICE,
+      /**
+       * Settlement offer.
+       */
+      SETTLEMENT,
+      /**
+       * Letter of guarantee.
+       */
+      LOG,
+    }
+  }
+  interface File {
+    hash: string;
+    name: string;
+    status: File.Status;
+    type: File.Type;
+  }
+
+  interface Insurer {
+    /**
+     * Name of insurance company.
+     */
+    name: string;
+    /**
+     * Deductible amount.
+     */
+    deductible: number;
+    /**
+     * Whether or not owner has gap coverage.
+     */
+    has_gap: boolean;
+  }
+
+  interface Vehicle {
+    /**
+     * The vehicle VIN.
+     */
+    vin: string;
+    /**
+     * The vehicle's miles.
+     */
+    miles: number;
+    /**
+     * Current location of the vehicle.
+     */
+    location: string;
+  }
 }
+
+// vim: set fdn=2 fdl=1:
