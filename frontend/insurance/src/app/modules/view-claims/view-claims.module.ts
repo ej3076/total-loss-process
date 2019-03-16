738ca57bc0c82ea9392cbe000704907eccae9318
Seamus Sloan <seamus@Seamuss-MacBook-Pro.local>
2019-03-16 18:36:44 -0400

    Login button now changes to Log Out and functions as such.

--- frontend/insurance/src/app/modules/view-claims/view-claims.module.ts
+++ frontend/insurance/src/app/modules/view-claims/view-claims.module.ts
@@ -4,25 +4,16 @@ import { AuthService } from '../../core/services/auth/auth.service';
 //import { MiddlewareService } from '../../core/services/middleware/middleware.service';
 import { MaterialModule } from '../material/material.module';
 import { CommonModule } from '@angular/common';
-import { EditClaimComponent } from '../../shared/components/edit-claim/edit-claim.component';
+//import { EditClaimComponent } from '../../shared/components/edit-claim/edit-claim.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ViewClaimsComponent
-  ],
-  imports: [
-    MaterialModule,
-    CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ViewClaimsComponent
-  ],
+  declarations: [ViewClaimsComponent],
+  imports: [MaterialModule, CommonModule, SharedModule],
+  exports: [ViewClaimsComponent],
   providers: [
     AuthService,
     //MiddlewareService
   ],
 })
 export class ViewClaimsModule {}
-
