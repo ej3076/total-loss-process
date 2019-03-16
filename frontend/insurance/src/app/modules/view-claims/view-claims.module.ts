9193056ccaa137f50d667539baed54be3e6f2323
Seamus Sloan <seamus@Seamuss-MacBook-Pro.local>
2019-03-16 18:11:35 -0400

    Added form validation Submit button works as designed Changed GAP Insurance buttons to checkbox

--- frontend/insurance/src/app/modules/view-claims/view-claims.module.ts
+++ frontend/insurance/src/app/modules/view-claims/view-claims.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ViewClaimsComponent } from './view-claims.component';
 import { AuthService } from '../../core/services/auth/auth.service';
-import { MiddlewareService } from '../../core/services/middleware/middleware.service';
+//import { MiddlewareService } from '../../core/services/middleware/middleware.service';
 import { MaterialModule } from '../material/material.module';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    ViewClaimsComponent
-  ],
-  imports: [
-    MaterialModule,
-    CommonModule
-  ],
-  exports: [
-    ViewClaimsComponent
-  ],
+  declarations: [ViewClaimsComponent],
+  imports: [MaterialModule, CommonModule],
+  exports: [ViewClaimsComponent],
   providers: [
     AuthService,
-    MiddlewareService
-  ]
+    //MiddlewareService
+  ],
 })
-export class ViewClaimsModule { }
+export class ViewClaimsModule {}
