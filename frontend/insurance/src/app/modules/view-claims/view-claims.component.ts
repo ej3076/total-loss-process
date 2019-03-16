4298a94f41d13840fce2894efd65649abc1c68ae
Jake Byrd <JakeByrd@quickenloans.com>
2019-03-16 16:18:04 -0400

    Fix new claim, add edit claim component

---
+++ frontend/insurance/src/app/modules/view-claims/view-claims.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MiddlewareService } from '../../core/services/middleware/middleware.service';
+import { MatTableDataSource } from '@angular/material';
+
+@Component({
+  selector: 'app-view-claims',
+  templateUrl: './view-claims.component.html',
+  styleUrls: ['./view-claims.component.scss']
+})
+export class ViewClaimsComponent implements OnInit {
+  claims: Protos.Claim[] = [];
+
+  displayedColumns: string[] = ['vin', 'dateOfLoss'];
+  dataSource: any;
+
+  constructor(private middlewareService: MiddlewareService) { }
+
+  ngOnInit() {
+    this.middlewareService.getClaims().subscribe(
+      result => {
+        this.claims = [...result];
+        this.dataSource = new MatTableDataSource(this.claims);
+      },
+      error => {
+        console.log(error);
+      },
+    );
+  }
+
+}
--- frontend/insurance/src/app/shared/shared.module.ts
+++ frontend/insurance/src/app/shared/shared.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { NewClaimComponent } from './components/new-claim/new-claim.component';
 import { MaterialModule } from '../modules/material/material.module';
+import { EditClaimComponent } from './components/edit-claim/edit-claim.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [NewClaimComponent],
-  imports: [MaterialModule],
-  exports: [NewClaimComponent],
+  declarations: [
+    NewClaimComponent,
+    EditClaimComponent
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule
+  ],
+  exports: [
+    NewClaimComponent
+  ]
 })
 export class SharedModule {}
