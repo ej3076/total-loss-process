a129fe44f142d7d62400f8e34717e09550e9c88d
Jake Byrd <JakeByrd@quickenloans.com>
2019-04-16 00:08:06 -0400

    Another one

--- frontend/insurance/src/app/core/services/middleware/middleware.service.spec.ts
+++ frontend/insurance/src/app/core/services/middleware/middleware.service.spec.ts
@@ -6,8 +6,45 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 
+const claimsResponse: Array<Protos.Claim> = [
+  {
+    status: 0,
+    created: '',
+    modified: '',
+    date_of_loss: '',
+    vehicle: {
+      vin: '1J4GA59178L553429',
+      miles: 123,
+      location: '',
+    },
+    insurer: {
+      name: '',
+      deductible: 1,
+      has_gap: false,
+    },
+    files: [],
+  },
+  {
+    status: 1,
+    created: '',
+    modified: '',
+    date_of_loss: '',
+    vehicle: {
+      vin: '1NXBR32E47Z851483',
+      miles: 321,
+      location: '',
+    },
+    insurer: {
+      name: '',
+      deductible: 1,
+      has_gap: false,
+    },
+    files: [],
+  },
+];
+
 describe('MiddlewareService', () => {
-  let service: MiddlewareService;
+  var service: MiddlewareService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,26 +61,6 @@ describe('MiddlewareService', () => {
 
   describe('getClaims', () => {
     it('should return an array of claims', () => {
-      const claimsResponse: Array<Protos.Claim> = [
-        {
-          status: 0,
-          created: '',
-          modified: '',
-          date_of_loss: '',
-          vehicle: {
-            vin: '1J4GA59178L553429',
-            miles: 123,
-            location: '',
-          },
-          insurer: {
-            name: '',
-            deductible: 1,
-            has_gap: false,
-          },
-          files: [],
-        },
-      ];
-
       let response;
       spyOn(service, 'getClaims').and.returnValue(of(claimsResponse));
 
@@ -52,6 +69,32 @@ describe('MiddlewareService', () => {
       });
 
       expect(response).toEqual(claimsResponse);
+    });
+  });
+
+  describe('getClaim', () => {
+    it('should return a single claim', () => {
+      let response;
+      spyOn(service, 'getClaim').and.returnValue(of(claimsResponse[1]));
+
+      service.getClaim('1NXBR32E47Z851483').subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(claimsResponse[1]);
+    });
+  });
+
+  describe('addClaim', () => {
+    it('should add a single claim and return it in response', () => {
+      let response;
+      spyOn(service, 'addClaim').and.returnValue(of(claimsResponse[0]));
+
+      service.addClaim(claimsResponse[0]).subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(claimsResponse[0]);
     });
   });
 });
