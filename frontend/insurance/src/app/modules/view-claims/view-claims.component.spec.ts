b6a097deb49e48deb72a0cbff51f301173283a6b
Jake Byrd <JakeByrd@quickenloans.com>
2019-04-17 20:00:52 -0400

    Error schemas

--- frontend/insurance/src/app/modules/view-claims/view-claims.component.spec.ts
+++ frontend/insurance/src/app/modules/view-claims/view-claims.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewClaimsComponent } from './view-claims.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MatTableModule, MatSortModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../core/services/auth/auth.service';
@@ -26,7 +26,7 @@ describe('ViewClaimsComponent', () => {
       ],
       providers: [AuthService, MiddlewareService],
       declarations: [ViewClaimsComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
