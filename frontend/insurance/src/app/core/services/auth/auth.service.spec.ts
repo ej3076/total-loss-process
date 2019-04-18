9870425ff294944082029a746ae6749b176d7990
ej3076 <46799635+ej3076@users.noreply.github.com>
2019-04-17 20:02:36 -0400

    Unit Test Temporary Fixes (#84)
    * Some temp fixes for unit tests
    
    * Error schemas

--- frontend/insurance/src/app/core/services/auth/auth.service.spec.ts
+++ frontend/insurance/src/app/core/services/auth/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AuthService', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       providers: [AuthService],
+      schemas: [NO_ERRORS_SCHEMA],
     }),
   );
 
